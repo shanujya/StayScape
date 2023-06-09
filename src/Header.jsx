7ca5361bd45ad321b3c915414a9a571838ca98fc
Shanujya Mishra <shanujyamishrajan94@gmail.com>
2023-06-09 17:52:48 +0530

    name change

--- src/Header.jsx
+++ src/Header.jsx
@@ -20,7 +20,7 @@ function Header() {
                 d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
               />
             </svg>
-            <span className="font-bold text-xl">airbnb</span>
+            <span className="font-bold text-xl">StayScape</span>
           </a>
           <div className="flex gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300 ">
             <div>Anywhere</div>
